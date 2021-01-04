3ce2c31d5c25116d45031855bc8fedca1c86f3d9
Darren Yakes <dayakes@gmail.com>
2021-01-04 10:57:01 -0500

    finished creating projects and some basic logic

---
+++ Palindrome.Client/Program.cs
@@ -0,0 +1,13 @@
+namespace Palindrome.Client
+{
+    public class Program
+    {
+        public static void main(String args[])
+        {
+            User user = new User();
+            user.GetInput();
+            user.IsPalindrome();
+            
+        }
+    }
+}
